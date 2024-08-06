2d2e010d4c5ebdf2a79bc97b15ebba24df438662
Alex <76778471+Zykkl@users.noreply.github.com>
2024-08-06 14:09:40 +0000

    37. exercise

--- 02-essentials/src/app/app.component.ts
+++ 02-essentials/src/app/app.component.ts
@@ -2,19 +2,21 @@ import { Component } from "@angular/core";
 
 import { DUMMY_USERS } from "./dummy-users";
 import { HeaderComponent } from "./header/header.component";
+import { TasksComponent } from "./tasks/tasks.component";
 import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  currentUserName = DUMMY_USERS[0].name;
 
-  onUserSelect(id: string) {
-    alert(`User ${id} selected`);
+  onUserSelect(name: string) {
+    this.currentUserName = name;
   }
 }
