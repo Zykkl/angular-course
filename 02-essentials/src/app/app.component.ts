cb6ea8c862119547af8fd531c5d0627d071f4ddb
Alex <76778471+Zykkl@users.noreply.github.com>
2024-08-06 13:56:36 +0000

    events and some handlers

--- 02-essentials/src/app/app.component.ts
+++ 02-essentials/src/app/app.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
+import { DUMMY_USERS } from "./dummy-users";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { DUMMY_USERS } from './dummy-users';
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [HeaderComponent, UserComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
   users = DUMMY_USERS;
+
+  onUserSelect(id: string) {
+    alert(`User ${id} selected`);
+  }
 }
--- 02-essentials/src/app/user/user.component.ts
+++ 02-essentials/src/app/user/user.component.ts
@@ -1,18 +1,23 @@
-import { Component, Input} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-user',
+  selector: "app-user",
   standalone: true,
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  templateUrl: "./user.component.html",
+  styleUrl: "./user.component.css",
 })
 export class UserComponent {
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  // avatar = input.required<string>();
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
 
   get imagePath() {
     return `assets/users/${this.avatar}`;
   }
   onUserCardClick() {
+    this.select.emit(this.id);
   }
 }
