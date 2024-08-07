1d6808c834c15ea2436629db52344b23a98aa454
Alexandre Carvalho <acarabineirocarvalho@hamilton.ch>
2024-08-07 16:22:16 +0200

    new TasksService, DI, implement TasksService methods

--- 02-essentials/src/app/tasks/new-task/new-task.component.ts
+++ 02-essentials/src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { NewTaskData } from "../task/task.model";
+import {TasksService} from "../tasks.service";
 
 @Component({
   selector: "app-new-task",
@@ -10,21 +10,26 @@ import { NewTaskData } from "../task/task.model";
   styleUrl: "./new-task.component.css",
 })
 export class NewTaskComponent {
-  @Output() cancelled = new EventEmitter<void>();
-  @Output() submitted = new EventEmitter<NewTaskData>();
+  @Input({required: true}) userId!: string;
+  @Output() close = new EventEmitter<void>();
   enteredTitle = "";
   enteredSummary = "";
   enteredDueDate = "";
+  constructor(private tasksService: TasksService) {}
 
   onCancel() {
-    this.cancelled.emit();
+    this.close.emit();
   }
 
   onSubmit() {
-    this.submitted.emit({
-      title: this.enteredTitle,
-      summary: this.enteredSummary,
-      date: this.enteredDueDate,
-    });
+    this.tasksService.addTask(
+      {
+        title: this.enteredTitle,
+        summary: this.enteredSummary,
+        date: this.enteredDueDate,
+      },
+      this.userId
+    );
+    this.close.emit();
   }
 }
--- 02-essentials/src/app/tasks/task/task.component.ts
+++ 02-essentials/src/app/tasks/task/task.component.ts
@@ -1,8 +1,9 @@
 import { DatePipe } from "@angular/common";
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 import { CardComponent } from "../../shared/card/card.component";
 import { Task } from "./task.model";
+import { TasksService } from "../tasks.service";
 
 @Component({
   selector: "app-task",
@@ -13,9 +14,10 @@ import { Task } from "./task.model";
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
-  @Output() onTaskComplete = new EventEmitter<string>();
+
+  constructor(private tasksService: TasksService) {}
 
   onCompleteTask() {
-    this.onTaskComplete.emit(this.task.id);
+    this.tasksService.removeTask(this.task.id);
   }
 }
