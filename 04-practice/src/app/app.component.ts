3d19908f546633dff3d2ea93808f73689b985536
Alexandre Carvalho <acarabineirocarvalho@hamilton.ch>
2024-08-08 12:16:02 +0200

    prettier fix

--- 04-practice/src/app/app.component.ts
+++ 04-practice/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import {HeaderComponent} from "./header/header.component";
-import {UserInputComponent} from "./user-input/user-input.component";
-import {InvestmentResultsComponent} from "./investment-results/investment-results.component";
+import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
   selector: 'app-root',
