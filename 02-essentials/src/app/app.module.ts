5ed8df4e4c924332ace141e4f6ea8fdba221450a
Alexandre Carvalho <acarabineirocarvalho@hamilton.ch>
2024-08-08 10:02:27 +0200

    new TasksModule and SharedModule

--- 02-essentials/src/app/app.module.ts
+++ 02-essentials/src/app/app.module.ts
@@ -2,25 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TasksComponent } from './tasks/tasks.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TaskComponent } from './tasks/task/task.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { CardComponent } from './shared/card/card.component';
-import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    TasksComponent,
-    UserComponent,
-    CardComponent,
-    TaskComponent,
-    NewTaskComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, UserComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, SharedModule, TasksModule],
 })
 export class AppModule {}
