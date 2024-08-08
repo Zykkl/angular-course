0da8b946cafb664cff874fc6eb8261c3fb54994c
Alexandre Carvalho <acarabineirocarvalho@hamilton.ch>
2024-08-08 09:32:57 +0200

    initial ngModule implementation

--- 02-essentials/src/app/app.component.ts
+++ 02-essentials/src/app/app.component.ts
@@ -1,16 +1,11 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
-import { DUMMY_USERS } from "./dummy-users";
-import { HeaderComponent } from "./header/header.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
-  selector: "app-root",
-  standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.css",
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
