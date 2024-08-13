7723102c24dd3724da57c45194404de2cc5e758d
Alexandre Carvalho <acarabineirocarvalho@hamilton.ch>
2024-08-13 15:07:23 +0200

    use tasks.service in the project

--- 09-services-di/src/app/tasks/tasks.service.ts
+++ 09-services-di/src/app/tasks/tasks.service.ts
@@ -1,20 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable, signal} from '@angular/core';
 import {Task, TaskStatus} from "./task.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
-  private tasks: Task[] = [];
+  private tasks = signal<Task[]>([]);
+  allTasks = this.tasks.asReadonly();
 
-  addTask(task: Task) {
-    this.tasks.push(task);
+  addTask(task: {title: string, description: string}) {
+    const newTask: Task = {
+      id: Math.random().toString(),
+      title: task.title,
+      description: task.description,
+      status: "OPEN"
+    }
+    this.tasks.update((oldTasks) => [...oldTasks, newTask]);
   }
 
-  getTasks(taskType?: TaskStatus) {
-    if (!taskType) {
-      return this.tasks;
+  getTasks(taskType: string) {
+    if (taskType === 'all') {
+      return this.tasks();
     }
-    return this.tasks.filter(task => task.status === taskType);
+    return this.tasks().filter(task => task.status === taskType);
+  }
+
+  updateTaskStatus(taskId: string, status: TaskStatus) {
+    this.tasks.update((oldTasks) => oldTasks.map((task) => task.id === taskId ? { ...task, status: status } : task));
   }
 }
