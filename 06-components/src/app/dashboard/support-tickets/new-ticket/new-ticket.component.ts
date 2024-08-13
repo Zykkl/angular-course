c7e18e6ab4248c47295664c319006b47fb893503
Alex <76778471+Zykkl@users.noreply.github.com>
2024-08-13 10:30:17 +0200

    06 Component Deep Dive Projects (#4)
    * init
    
    * header component
    
    * create components for dashboard items
    
    dont judge me, i just followed whatever the dood did in the course
    
    * dashboard-item component
    
    took him a while, but he got it :D
    
    * ticket, new-ticket component
    
    * button shared component, someone please explain how this benefits
    
    * fallback content (just for notetaking)
    
    * remove to avoid error
    
    * wth
    
    * holy shite it just took em a long while
    
    * fix styling issues
    
    * remove obsolete wrappers by using :host
    
    * state
    
    * state (currently throws)
    
    * use viewChild decorator to get elementRef to form in new-ticket.component
    
    * init
    
    * custom twoway binding
    
    * better, easier twoway binding
    
    ---------
    
    Co-authored-by: Alexandre Carvalho <acarabineirocarvalho@hamilton.ch>

---
+++ 06-components/src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
@@ -0,0 +1,23 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ButtonComponent } from '../../../shared/button/button.component';
+import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-ticket',
+  standalone: true,
+  imports: [ButtonComponent, ControlComponent, FormsModule],
+  templateUrl: './new-ticket.component.html',
+  styleUrl: './new-ticket.component.css',
+})
+export class NewTicketComponent {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+
+  onSubmit(title: string, description: string) {
+    console.log({
+      title: title,
+      description: description,
+    });
+    this.form?.nativeElement.reset();
+  }
+}
