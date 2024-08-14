29928cdfbff886834e0f2739cf775869a71a0f49
Alex <76778471+Zykkl@users.noreply.github.com>
2024-08-14 09:54:44 +0200

    12 HTTP requests project (#10)
    * init
    
    * populate available-places with backend data
    
    * loading text
    
    * handle errors
    
    * add user places to json file, show user places
    
    * places.service
    
    * error handling in optimistic updating
    
    * add error modal, show errors
    
    * remove user places feat
    
    * finalize
    
    ---------
    
    Co-authored-by: Alexandre Carvalho <acarabineirocarvalho@hamilton.ch>

---
+++ 12-http-requests/src/app/places/available-places/available-places.component.ts
@@ -0,0 +1,50 @@
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+
+import { Place } from '../place.model';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { PlacesComponent } from '../places.component';
+import { PlacesService } from '../places.service';
+
+@Component({
+  selector: 'app-available-places',
+  standalone: true,
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
+})
+export class AvailablePlacesComponent implements OnInit {
+  places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  errorMsg = signal('');
+  private placesService = inject(PlacesService);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    this.isFetching.set(true);
+    const subscription = this.placesService.loadAvailablePlaces().subscribe({
+      next: (resData) => {
+        this.places.set(resData);
+      },
+      error: (error: Error) => {
+        console.log(error);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  onSelectPlaces(place: Place) {
+    const subscription = this.placesService
+      .addPlaceToUserPlaces(place)
+      .subscribe();
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+}
---
+++ 12-http-requests/src/app/places/user-places/user-places.component.ts
@@ -0,0 +1,44 @@
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+
+import { Place } from '../place.model';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { PlacesComponent } from '../places.component';
+import { PlacesService } from '../places.service';
+
+@Component({
+  selector: 'app-user-places',
+  standalone: true,
+  templateUrl: './user-places.component.html',
+  styleUrl: './user-places.component.css',
+  imports: [PlacesContainerComponent, PlacesComponent],
+})
+export class UserPlacesComponent implements OnInit {
+  private placesService = inject(PlacesService);
+  private destroyRef = inject(DestroyRef);
+  places = this.placesService.loadedUserPlaces;
+  isFetching = signal(false);
+  errorMsg = signal('');
+
+  ngOnInit() {
+    this.isFetching.set(true);
+    const subscription = this.placesService.loadUserPlaces().subscribe({
+      error: (error: Error) => {
+        this.errorMsg.set(error.message);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+  onRemovePlace(place: Place) {
+    const subsciption = this.placesService.removeUserPlace(place).subscribe();
+
+    this.destroyRef.onDestroy(() => {
+      subsciption.unsubscribe();
+    });
+  }
+}
